056721bf7b51223c5aeecdde60c9844ad4d5ed56
Tissa Khosla <tissa.music@gmail.com>
2019-10-13 17:36:04 -0400

    game completion logic?

--- game-logic.js
+++ game-logic.js
@@ -58,22 +58,25 @@ console.log(playerOne, playerTwo);
 function war(index) {
 	if (playerOne[index] > playerTwo[index]) {
 		console.log("--WAR--ONE--")
-		playerOne = playerOne.concat(playerTwo.splice(0,4))
+		playerOne = playerOne.concat(playerTwo.splice(0,index+1))
 		playerOne.push(playerOne.shift())
 	} else if (playerTwo[index] > playerOne[index]) {
 			console.log("--WAR--TWO--")
-			playerTwo = playerTwo.concat(playerOne.splice(0,4))
-			playerTwo.concat(playerTwo.shift())
-		} else {
-				console.log("AGAIN!")
-			}
+			playerTwo = playerTwo.concat(playerOne.splice(0,index+1))
+			playerTwo.push(playerTwo.shift())
+	} else {
+			console.log("here!")
+			war(index+4);
+	}
 	console.log(playerOne, playerTwo);
 }
 
 
 
 // BUILD v4
-for (var i = 1; i <= 20; i++) {
+// for (var i = 1; i <= 100; i++) {
+var i = 0
+while(playerOne.length != 0 && playerTwo.length != 0) {
 	if (playerOne[0] === playerTwo[0]) {
 		console.log("___o_0--->WAR<---O_o___")
 		war(3);
@@ -83,13 +86,14 @@ for (var i = 1; i <= 20; i++) {
 			playerOne.push(playerTwo.shift());
 			playerOne.push(playerOne.shift());
 			console.log(playerOne, playerTwo);
-		} else if (playerTwo[0] > playerOne[0]) {
-				console.log("-----*Two*-----")
-				playerTwo.push(playerOne.shift());
-				playerTwo.push(playerTwo.shift());
-				console.log(playerOne, playerTwo);
-			}
-console.log(`turn ${i}`)
+	} else if (playerTwo[0] > playerOne[0]) {
+			console.log("-----*Two*-----")
+			playerTwo.push(playerOne.shift());
+			playerTwo.push(playerTwo.shift());
+			console.log(playerOne, playerTwo);
+	}
+	i++;
+	console.log(`turn ${i}`)
 }
 
 
