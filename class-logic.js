a69d312bc821c4fa5fe1aa5784f7620ab32d4512
Tissa Khosla <tissa.music@gmail.com>
2019-10-28 14:18:24 -0400

    added message to browser

--- class-logic.js
+++ class-logic.js
@@ -18,8 +18,8 @@ class Game {
 		var playerOne = [];
 		var playerTwo = [];
 		for(var i = 1; i <= 26 ; i++) {
-		playerOne.unshift(deck.shift())
-		playerTwo.unshift(deck.shift())
+         playerOne.unshift(deck.shift())
+         playerTwo.unshift(deck.shift())
 		}
 		//test
 		console.log("The Deal")
@@ -28,26 +28,26 @@ class Game {
 
 		//WAR function
 		function war(index) {
-		console.log("___o_0--->WAR<---O_o___")
-		// if (index > 3) {
-		// 		alert(`${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`);
-		// }
-		index = Math.min(index, Math.min(playerOne.length, playerTwo.length) - 1)
-		// if (index > 3) {
-		// 		alert(`${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`)
-		// }
-		if (playerOne[index] > playerTwo[index]) {
-			playerOne = playerOne.concat(playerTwo.splice(0,index+1));
-			playerOne.push(playerOne.shift());
-		} else if (playerTwo[index] > playerOne[index]) {
-				playerTwo = playerTwo.concat(playerOne.splice(0,index+1));
-				playerTwo.push(playerTwo.shift());
-		} else {
-				if(index === playerOne.length - 1 || index === playerTwo.length - 1) {
-					alert(`Game over\n${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`);
-				}
-				war(index+4) 
-		}
+         console.log("___o_0--->WAR<---O_o___")
+         // if (index > 3) {
+         // 		alert(`${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`);
+         // }
+         index = Math.min(index, Math.min(playerOne.length, playerTwo.length) - 1)
+         // if (index > 3) {
+         // 		alert(`${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`)
+         // }
+         if (playerOne[index] > playerTwo[index]) {
+            playerOne = playerOne.concat(playerTwo.splice(0,index+1));
+            playerOne.push(playerOne.shift());
+         } else if (playerTwo[index] > playerOne[index]) {
+               playerTwo = playerTwo.concat(playerOne.splice(0,index+1));
+               playerTwo.push(playerTwo.shift());
+         } else {
+               if(index === playerOne.length - 1 || index === playerTwo.length - 1) {
+                  alert(`The Game ended within War!!\n${index}\n${playerOne.length} --- ${playerOne}\n${playerTwo.length} --- ${playerTwo}`);
+               }
+               war(index+4) 
+		      }
 		// console.log(playerOne, playerTwo);
 		// console.log("player 1 has " + playerOne.length + " cards.")
 		// console.log("player 2 has " + playerTwo.length + " cards.")
@@ -57,30 +57,37 @@ class Game {
 		var i = 0
 
 		while(playerOne.length != 0 && playerTwo.length != 0) {
-		if (playerOne[0] === playerTwo[0]) {
-			war(3);
-			// console.log(playerOne, playerTwo);
-		} else if (playerOne[0] > playerTwo[0]) {
-				console.log("-----*One*-----");
-				playerOne.push(playerTwo.shift());
-				playerOne.push(playerOne.shift());
-				console.log(playerOne, playerTwo);
-		} else if (playerTwo[0] > playerOne[0]) {
-				console.log("-----*Two*-----")
-				playerTwo.push(playerOne.shift());
-				playerTwo.push(playerTwo.shift());
-				console.log(playerOne, playerTwo);
-		}
+         if (playerOne[0] === playerTwo[0]) {
+            war(3);
+            // console.log(playerOne, playerTwo);
+         } else if (playerOne[0] > playerTwo[0]) {
+               console.log("-----*One*-----");
+               playerOne.push(playerTwo.shift());
+               playerOne.push(playerOne.shift());
+               console.log(playerOne, playerTwo);
+         } else if (playerTwo[0] > playerOne[0]) {
+               console.log("-----*Two*-----")
+               playerTwo.push(playerOne.shift());
+               playerTwo.push(playerTwo.shift());
+               console.log(playerOne, playerTwo);
+         }
 		i++;
 
 		console.log(`turn ${i}`)
 		console.log(playerOne, playerTwo);
 		console.log("player 1 has " + playerOne.length + " cards.")
-		console.log("player 2 has " + playerTwo.length + " cards.")
+      console.log("player 2 has " + playerTwo.length + " cards.")
 
-		}
+      }
+   let body = document.querySelector("body")
+   console.dir(body)
+   let status = document.createElement("h2")
+   status.innerText = `After ${i} turns, player 1 has ${playerOne.length} cards and player 2 has ${playerTwo.length} cards. `
+   body.appendChild(status)
+
 	}
 }
 
 var war = new Game();
+console.log(war.gameStart())
 
